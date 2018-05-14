5c8171814a105e84fa77f339598bf9e1729a85fd
Jaakko Veijonen <vekkari@gmail.com>
2018-05-14 14:17:34 +0300

    EXPERIMENTAL run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo salt-run winrepo.update_git_repos && sudo salt -G 'os:windows' pkg.refresh_db && sudo salt -G 'os:windows' state.apply windowsgames
+sudo mkdir -p /srv/salt && sudo mv windowsgames/ /srv/salt/windowsgames/ && sudo salt-run winrepo.update_git_repos && sudo salt -G 'os:windows' pkg.refresh_db && sudo salt -G 'os:windows' state.apply windowsgames
